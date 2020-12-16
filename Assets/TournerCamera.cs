0e9a97db10fb901122fc843a9dcbbe4252ee86a2
Gui-Vez <vezina.guillaume@outlook.com>
2020-12-16 18:06:14 -0500

    Ajout Projet Unity

---
+++ Assets/TournerCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TournerCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Coucou :)
+        // lol
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0.25f, 0);
+    }
+}
