7490ae39c8d67e5e453fb2017a9957f002478395
Gui-Vez <vezina.guillaume@outlook.com>
2020-12-16 18:29:45 -0500

    Script qui fait tout bugger

---
+++ Assets/ScriptInutile.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptInutile : MonoBehaviour
+{
+    // Ceci est un code qui cause bein des erreurs oh nooooooooooo
+}
