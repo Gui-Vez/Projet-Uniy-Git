311588535ad9a0abd008639da818b70b4ed6321d
Gui-Vez <vezina.guillaume@outlook.com>
2020-12-16 18:21:28 -0500

    Changement Script Camera

--- Assets/TournerCamera.cs
+++ Assets/TournerCamera.cs
@@ -7,13 +7,18 @@ public class TournerCamera : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // Coucou :)
-        // lol
+
+        /********************************************************/
+        /*                  CECI EST UN TEST                    */
+        /* Si tu vois ceci, c'est que ce dépôt est plus récent. */
+        /********************************************************/
+
+        // jai faaaaiiiiim
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0.25f, 0);
+        transform.Rotate(0, 0.2f, 0);
     }
 }
